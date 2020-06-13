564305cad009147d6d5b0574a6d4d9b2e0b96853
FURKAN AKGUN <fakgun@yukselproje.com.tr>
2020-06-13 17:53:49 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<DicePage> {
+  int leftBuildNumber = 1;
+  int rightBuildNubmer = 1;
+
+  void changeDice() {
+   setState(() {
+                  leftBuildNumber = Random().nextInt(6) + 1;
+                  rightBuildNubmer = Random().nextInt(6) + 1;
+                });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDice();
+              },
+              child: Image.asset('images/dice$leftBuildNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDice();
+              },
+              child: Image.asset('images/dice$rightBuildNubmer.png'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
